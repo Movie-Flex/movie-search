ae9c70750f57ccbf80d4527a7791ce620625991b
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-22 21:40:17 +0530

    login fiels set to both username or email

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -33,18 +33,18 @@ const Login = () => {
             <form class="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
-                  for="email"
+                  for="userId"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Your Email/Username
                 </label>
                 <input
                   onChange={(e) =>
-                    setLoginData({ ...loginData, email: e.target.value })
+                    setLoginData({ ...loginData, userId: e.target.value })
                   }
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="userId"
+                  name="userId"
+                  id="userId"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
