741691b470a5596ebed4c776218d38fe5a495a74
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-24 11:23:04 +0530

    making th eshow and hide visibility of password

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSignup } from "../hooks/useSignUp";
 import checkStrongNess from "../utils/PasswordCheck";
+import { useStatStyles } from "@chakra-ui/react";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -17,10 +18,14 @@ const Signup = () => {
   });
 
   const [passwordStrength, setPasswordStrength] = useState("Weak");
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
 
   const handleSignup = async (e, signupData) => {
     e.preventDefault();
-    // console.log(signupData);
     try {
       await signup(signupData);
     } catch (error) {
@@ -101,20 +106,46 @@ const Signup = () => {
                 >
                   Password
                 </label>
+                <div className="relative">
+                  
                 <input
                   onChange={(e) => {
                     setSignupData({ ...signupData, password: e.target.value });
                     setPasswordStrength(checkStrongNess(e.target.value));
                   }}
-                  type="password"
+                  type={show ? 'text' : 'password'}
                   name="password"
                   id="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600
                    focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
                     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
+                    required=""
+                    />
+
+<div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+      <svg
+        className={`h-6 text-gray-700 ${show ? 'hidden' : 'block'}`}
+        fill="none"
+        onClick={toggleShow}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+      >
+        <path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z" />
+      </svg>
+
+      <svg
+        className={`h-6 text-gray-700 ${show ? 'block' : 'hidden'}`}
+        fill="none"
+        onClick={toggleShow}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 640 512"
+      >
+        <path fill="currentColor" d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z" />
+      </svg>
+    </div>
+               
+                    </div>
                 <p className="text-sm justify-items-end font-light flex  text-gray-500 dark:text-gray-400">
                   Password strength:{" "}
                   <span
@@ -129,6 +160,7 @@ const Signup = () => {
                     {passwordStrength}
                   </span>
                 </p>
+               
               </div>
               <div>
                 <label
@@ -184,15 +216,15 @@ const Signup = () => {
                   handleSignup(e, signupData);
                 }}
                 type="submit"
-                class="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800  text-black dark:text-white"
               >
                 Create an account
               </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p class="text-sm font-light text-black dark:text-white">
                 Already have an account?{" "}
                 <Link
                   to="/login"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  class="font-medium  text-black dark:text-white hover:underline "
                 >
                   Login here
                 </Link>
