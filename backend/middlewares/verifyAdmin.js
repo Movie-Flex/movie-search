7d4fdbcc3761d577447986ccd4f188ab7b556d1a
Prathmesh <pathubhai@kgpian.iitkgp.ac.in>
2024-03-28 20:39:30 +0530

    Added authentication in the generate admin token

--- backend/middlewares/verifyAdmin.js
+++ backend/middlewares/verifyAdmin.js
@@ -1,27 +1,43 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { isAdmin } = require('./admin');
 
-
-const generateAdminToken = (req, res) => {
-    try {
-        const {clientEmail, adminEmail, role} = req.body
-        const payload = {
-            clientEmail : clientEmail,
-            adminEmail  : adminEmail,
-            role: role ,
-            issuedAt: Date.now()
-        };
-
-        const options = {
-            expiresIn: process.env.EXPIRES_IN || '100m'
-        };
-        
-        const adminToken = jwt.sign(payload, process.env.SECRET_KEY, options);
-        return res.status(401).json({adminToken} ); 
-    } catch (err) {
-        console.error("Error occurred while generating admin token:", err);
-        throw err; 
+const generateAdminToken = async (req, res) => {
+  try {
+    const bearer = req.headers['authorization'];
+    if (!bearer) {
+      return res.status(404).json({ error: 'No authentication token' });
     }
+    const token = bearer.split(" ")[1];
+    if (!token) {
+      return res.status(404).json({ error: 'No authentication token found' });
+    }
+    //console.log(token);
+    let role_check = false;
+    //console.log(role);
+    role_check = await isAdmin(token);
+    //console.log(role);
+    if (!role) {
+      return res.status(400).json({ error: 'You dont have permission to perform this operation' });
+    }
+    const { clientEmail, adminEmail, role } = req.body
+    const payload = {
+      clientEmail: clientEmail,
+      adminEmail: adminEmail,
+      role: role,
+      issuedAt: Date.now()
+    };
+
+    const options = {
+      expiresIn: process.env.EXPIRES_IN || '100m'
+    };
+
+    const adminToken = jwt.sign(payload, process.env.SECRET_KEY, options);
+    return res.status(401).json({ adminToken });
+  } catch (err) {
+    console.error("Error occurred while generating admin token:", err);
+    throw err;
+  }
 };
 
 const verifyAdminToken = (adminToken) => {
@@ -32,6 +48,7 @@ const verifyAdminToken = (adminToken) => {
   } catch (err) {
     console.log("error verifying admin token.", err)
     return null;
-  }}
+  }
+}
 
-module.exports = {generateAdminToken, verifyAdminToken};
+module.exports = { generateAdminToken, verifyAdminToken };
