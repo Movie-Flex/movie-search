93a7c3c17f3d972e6a3d242f49fb64b238d80202
ravi-ivar-7 <ravikuamr117211@gmail.com>
2024-03-28 17:27:00 +0530

    added genres to fuzzysearch

--- backend/controllers/fuzzySearch.js
+++ backend/controllers/fuzzySearch.js
@@ -7,33 +7,46 @@ const fuzzySearch = async (req, res) => {
   let client;
   try {
     const  query  = req.query.q;
+    const genres = req.query.g;
 
     if (!query) {
       return res.status(209).json({ message: 'Query is required' });
     }
-
-    // Connect to the database and get client and collection
     const { client: connectedClient, collection } = await connectToDatabase(dbName, collectionName); 
-    client = connectedClient; // Assign client
-
-    // Create the aggregation pipeline with the $search stage
-    const pipeline = [
-      {
-        $search: {
-          index: 'default',
-          text: {
-            query: query,
-            path: 'title',
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
-            }
+    client = connectedClient; 
+    const searchStage = {
+      $search: {
+        index: 'default',
+        text: {
+          query: query,
+          path: 'title',
+          fuzzy: {
+            maxEdits: 2,
+            prefixLength: 3
           }
         }
       }
-    ];
+    };
+    const pipeline = [searchStage];
+    // If genre filter is provided, add a match stage to filter by genre
+    if (genres) {
+      const matchStage = {
+        $match: {
+          genres: { $in: Array.isArray(genres) ? genres : [genres] }
+        }
+      };
+      pipeline.push(matchStage);
+    }
 
+    const projectStage = {
+      $project: {
+        plot_embedding: 0
+      }
+    };
+    pipeline.push(projectStage);
     const movies = await collection.aggregate(pipeline).toArray();
+
+
 
     res.status(200).json(movies);
   } catch (err) {
