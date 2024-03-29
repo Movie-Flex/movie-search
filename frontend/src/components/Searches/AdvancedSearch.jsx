19eeb8270278b41b0b6cddceb1363faf3013ec9c
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-29 17:27:04 +0530

    fixed the bug in the  form

--- frontend/src/components/Searches/AdvancedSearch.jsx
+++ frontend/src/components/Searches/AdvancedSearch.jsx
@@ -8,7 +8,7 @@ export default function AdvancedSearch({ searchResult }) {
             <h1 class="text-3xl text-center text-white">Advanced Search</h1>
            {
             (searchResult && searchResult.length >0 ) ? (
-              <div class="grid grid-cols-1 gap-4 mt-4">
+              <div class="grid grid-cols-1 gap-8 mt-4">
               {searchResult.map((movie) => (
                   <MovieCard key={movie.id} movie={movie} />
               ))}
