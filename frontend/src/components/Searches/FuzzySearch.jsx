66acb084be2a352169a302c1377a6ec9fc2d0aaa
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-29 17:28:54 +0530

    removed a line of fuzzy search

--- frontend/src/components/Searches/FuzzySearch.jsx
+++ frontend/src/components/Searches/FuzzySearch.jsx
@@ -4,7 +4,7 @@ export default function FuzzySearch({ searchResult }) {
     return(
         <div class="min-h-screen grid place-items-center font-mono bg-gray-900">
         
-            <h1 class="text-3xl text-center text-white">Fuzzy Search</h1>
+            {/* <h1 class="text-3xl text-center text-white">Fuzzy Search</h1> */}
            {
             (searchResult && searchResult.length >0 ) ? (
               <div class="grid grid-cols-1 gap-4 mt-4">
