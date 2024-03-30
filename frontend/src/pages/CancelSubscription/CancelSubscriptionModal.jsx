35d3e9a4c824d5bab437151e33b98ca668767417
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-30 12:48:47 +0530

    removed the no button if the yes cancel is clicked

--- frontend/src/pages/CancelSubscription/CancelSubscriptionModal.jsx
+++ frontend/src/pages/CancelSubscription/CancelSubscriptionModal.jsx
@@ -60,14 +60,14 @@ export default function CancelSubscriptionModal({ closeModal, refundInfo ,refund
           <CancelSubscriptionDetailsForm refundInfo={refundInfo} />
 
           <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
-            <button
+           {!loading && <button
               className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
               type="button"
               style={{ transition: "all .15s ease" }}
               onClick={() => closeModal(false)}
             >
               No
-            </button>
+            </button>}
 
             <button
               className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
